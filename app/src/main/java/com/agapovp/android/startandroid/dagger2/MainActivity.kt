e1c689d01373857373dc97c40f5445efaaafd2e4
p-agapov <p.a.agapov@gmail.com>
2020-06-24 23:13:44 +0300

    Initial commit

---
+++ app/src/main/java/com/agapovp/android/startandroid/dagger2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agapovp.android.startandroid.dagger2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
